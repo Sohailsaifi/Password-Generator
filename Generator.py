1fe797d17ddef4fef2d256e62db3acebfaa18390
Sohail Saifi <sohailsaifi65@gmail.com>
2020-08-30 15:40:39 +0530

    Completed

---
+++ Generator.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+if __name__ == "__main__":
+    
+    s1 = string.ascii_lowercase
+
+    s2 = string.ascii_uppercase
+
+    s3 = string.digits
+
+    s4 = string.punctuation
+
+    pass_len = int(input("Enter the length of password : \n"))
+
+    s = []
+
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+
+    random.shuffle(s)
+
+    password = "".join(s[0:pass_len])
+
+    print("Your password is :   " + password + "\n")
